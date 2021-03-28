f43397fd287688aba25e8f2ff0472511b0d38531
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-03-27 20:01:03 -0700

    add mem observance

--- architect.py
+++ architect.py
@@ -154,7 +154,7 @@ def meta_learn(model, optimizer, input, target, input_val, target_val, coefficie
             #weights = calc_instance_weights(input, target, input_val, target_val, logits_val, coefficient_vector, visual_encoder)
             loss = F.cross_entropy(logits, target, reduction='none')
             weights = torch.ones(loss.shape).to("cuda")
-            weighted_training_loss = loss#torch.mean(weights * loss)
+            weighted_training_loss = torch.mean(weights * loss)
             foptimizer.step(weighted_training_loss)  # replaces gradients with respect to model weights -> w2
 
             logits_val = fmodel(input_val)
