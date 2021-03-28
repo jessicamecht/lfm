cad6d9102d77ca71c53503cd0d63376e93ce01cf
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-03-27 19:19:15 -0700

    add mem observance

--- config.py
+++ config.py
@@ -51,7 +51,7 @@ class SearchConfig(BaseConfig):
                             help='weight decay for weights')
         parser.add_argument('--w_grad_clip', type=float, default=5.,
                             help='gradient clipping for weights')
-        parser.add_argument('--print_freq', type=int, default=50, help='print frequency')
+        parser.add_argument('--print_freq', type=int, default=1, help='print frequency')
         parser.add_argument('--gpus', default='0', help='gpu device ids separated by comma. '
                             '`all` indicates use all gpus.')
         parser.add_argument('--epochs', type=int, default=50, help='# of training epochs')
@@ -87,7 +87,7 @@ class AugmentConfig(BaseConfig):
         parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
         parser.add_argument('--grad_clip', type=float, default=5.,
                             help='gradient clipping for weights')
-        parser.add_argument('--print_freq', type=int, default=10, help='print frequency')
+        parser.add_argument('--print_freq', type=int, default=1, help='print frequency')
         parser.add_argument('--gpus', default='0', help='gpu device ids separated by comma. '
                             '`all` indicates use all gpus.')
         parser.add_argument('--epochs', type=int, default=600, help='# of training epochs')
