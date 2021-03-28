ebad6686708b2465db7df5f824d45e27d6ac1b4e
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-03-27 20:55:08 -0700

    add mem observance

--- architect.py
+++ architect.py
@@ -167,21 +167,11 @@ def meta_learn(model, optimizer, input, target, input_val, target_val, coefficie
             #visual_encoder_gradients = (visual_encoder_gradients[0].detach(), visual_encoder_gradients[1].detach())# equivalent to backward for given parameters
             logits.detach()
             weighted_training_loss.detach()
-        recursive_del(fmodel.modules())
+        mem_report()
         del logits, meta_val_loss, foptimizer, fmodel, weighted_training_loss, logits_val, weights,
         gc.collect()
         torch.cuda.empty_cache()
     #return visual_encoder_gradients, coeff_vector_gradients
-
-def recursive_del(modules):
-    children = list(modules)
-    print(children)
-    if not children:
-        if isinstance(modules, nn.Linear) or isinstance(modules, nn.Embedding) or isinstance(modules, nn.Conv2d):
-            del modules.weight
-    else:
-        for child in children:
-            recursive_del(child)
 
 
 def update_gradients(visual_encoder_gradients, coeff_vector_gradients, visual_encoder, coefficient_vector):
@@ -198,3 +188,62 @@ def update_gradients(visual_encoder_gradients, coeff_vector_gradients, visual_en
                 p.grad += grad.detach()
             else:
                 p.grad = grad.detach()
+
+
+import gc
+
+import torch
+
+## MEM utils ##
+def mem_report():
+    '''Report the memory usage of the tensor.storage in pytorch
+    Both on CPUs and GPUs are reported'''
+
+    def _mem_report(tensors, mem_type):
+        '''Print the selected tensors of type
+        There are two major storage types in our major concern:
+            - GPU: tensors transferred to CUDA devices
+            - CPU: tensors remaining on the system memory (usually unimportant)
+        Args:
+            - tensors: the tensors of specified type
+            - mem_type: 'CPU' or 'GPU' in current implementation '''
+        print('Storage on %s' %(mem_type))
+        print('-'*LEN)
+        total_numel = 0
+        total_mem = 0
+        visited_data = []
+        for tensor in tensors:
+            if tensor.is_sparse:
+                continue
+            # a data_ptr indicates a memory block allocated
+            data_ptr = tensor.storage().data_ptr()
+            if data_ptr in visited_data:
+                continue
+            visited_data.append(data_ptr)
+
+            numel = tensor.storage().size()
+            total_numel += numel
+            element_size = tensor.storage().element_size()
+            mem = numel*element_size /1024/1024 # 32bit=4Byte, MByte
+            total_mem += mem
+            element_type = type(tensor).__name__
+            size = tuple(tensor.size())
+
+            print('%s\t\t%s\t\t%.2f' % (
+                element_type,
+                size,
+                mem) )
+        print('-'*LEN)
+        print('Total Tensors: %d \tUsed Memory Space: %.2f MBytes' % (total_numel, total_mem) )
+        print('-'*LEN)
+
+    LEN = 65
+    print('='*LEN)
+    objects = gc.get_objects()
+    print('%s\t%s\t\t\t%s' %('Element type', 'Size', 'Used MEM(MBytes)') )
+    tensors = [obj for obj in objects if torch.is_tensor(obj)]
+    cuda_tensors = [t for t in tensors if t.is_cuda]
+    host_tensors = [t for t in tensors if not t.is_cuda]
+    _mem_report(cuda_tensors, 'GPU')
+    _mem_report(host_tensors, 'CPU')
+    print('='*LEN)
